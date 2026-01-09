1710d7699f321c6eb54091b1a856857d8b9bdc15
Isamar Zhu <izhu@mit.edu>
2026-01-09 09:42:12 -0800

    Added import video modal 2

--- src/components/layout/AppFrame.jsx
+++ src/components/layout/AppFrame.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { AppShell } from "@mantine/core";
+import { VideoProvider } from "../../contexts/VideoContext";
+import ImportModal from "../overlays/ImportModal";
 import HeaderMenuBar from "./HeaderMenuBar";
 import MainColumn from "./MainColumn";
 
 export default function AppFrame() {
+    const [importModalOpened, setImportModalOpened] = useState(false); // track import modal state across app
+
     return (
         <AppShell
             header={{ height: 44 }}
-            padding="md"
+            padding="sm"
             styles={{
                 header: {
                     backgroundColor: "#f1f3f5",   // light grey
-                    borderBottom: "1px solid #dee2e6",
+                    borderBottom: "2px solid #dee2e6",
                 },
                 main: {
                     height: "calc(100vh - 44px)",
@@ -18,11 +23,17 @@ export default function AppFrame() {
             }}
         >
             <AppShell.Header>
-                <HeaderMenuBar />
+                <HeaderMenuBar onImportClick={() => setImportModalOpened(true)} />
             </AppShell.Header>
 
             <AppShell.Main>
-                <MainColumn />
+                <VideoProvider> {/* provide video context to main content, don't need to wrap around entire appshell */}
+                    <MainColumn />
+                    <ImportModal
+                        opened={importModalOpened}
+                        onClose={() => setImportModalOpened(false)}
+                    />
+                </VideoProvider>
             </AppShell.Main>
         </AppShell>
     );
